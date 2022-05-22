b7eb05a4718c03fe179fc076db88849ad3c3c572
Simon Van Accoleyen <simon@vanacco.fr>
2022-05-22 21:23:26 +0200

    Clickable admin widgets

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -33,36 +33,66 @@ class AdminController extends AbstractController {
     #[Route('/widget/orders', name: 'admin_widget_orders', methods: ['GET'])]
     public function ordersCountWidget(OrderRepository $repository) {
         $count = $repository->count([]);
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Orders'), 'count' => $count, 'icon' => 'fas fa-receipt']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Orders'),
+            'count' => $count,
+            'icon' => 'fas fa-receipt',
+            'link' => $this->generateUrl('order_admin_index')
+        ]);
     }
 
     #[Route('/widget/orders_today', name: 'admin_widget_orders_today', methods: ['GET'])]
     public function todayOrdersCountWidget(OrderRepository $repository) {
         $count = $repository->countAllToday();
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Orders today'), 'count' => $count, 'icon' => 'fas fa-receipt']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Orders today'),
+            'count' => $count,
+            'icon' => 'fas fa-receipt',
+            'link' => $this->generateUrl('order_admin_index')
+        ]);
     }
     
     #[Route('/widget/cocktails', name: 'admin_widget_cocktails', methods: ['GET'])]
     public function cocktailsCountWidget(CocktailRepository $repository) {
         $count = $repository->count([]);
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Cocktails'), 'count' => $count, 'icon' => 'fas fa-martini-glass']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Cocktails'),
+            'count' => $count,
+            'icon' => 'fas fa-martini-glass',
+            'link' => $this->generateUrl('cocktail_admin_index')
+        ]);
     }
 
     #[Route('/widget/cocktails_available', name: 'admin_widget_cocktails_available', methods: ['GET'])]
     public function availableCocktailsCountWidget(CocktailRepository $repository) {
         $count = $repository->countAllAvailable();
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Available cocktails'), 'count' => $count, 'icon' => 'fas fa-martini-glass']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Available cocktails'),
+            'count' => $count,
+            'icon' => 'fas fa-martini-glass',
+            'link' => $this->generateUrl('cocktail_admin_index')
+        ]);
     }
 
     #[Route('/widget/ingredients', name: 'admin_widget_ingredients', methods: ['GET'])]
     public function ingredientsCountWidget(IngredientRepository $repository) {
         $count = $repository->count([]);
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Ingredients'), 'count' => $count, 'icon' => 'fas fa-wine-bottle']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Ingredients'),
+            'count' => $count,
+            'icon' => 'fas fa-wine-bottle',
+            'link' => $this->generateUrl('ingredient_admin_index')
+        ]);
     }
 
     #[Route('/widget/ingredients_available', name: 'admin_widget_ingredients_available', methods: ['GET'])]
     public function availableIngredientsCountWidget(Request $request, IngredientRepository $repository) {
         $count = $repository->count(['isInStock' => true]);
-        return $this->render('admin/widgets/_simple_count.html.twig', ['label' => $this->translator->trans('Available ingredients'), 'count' => $count, 'icon' => 'fas fa-wine-bottle']);
+        return $this->render('admin/widgets/_simple_count.html.twig', [
+            'label' => $this->translator->trans('Available ingredients'),
+            'count' => $count,
+            'icon' => 'fas fa-wine-bottle',
+            'link' => $this->generateUrl('ingredient_admin_index')
+        ]);
     }
 }
