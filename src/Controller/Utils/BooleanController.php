c604c9fe40aa26916832b0765471894b9dd20908
Simon Van Accoleyen <simon@vanacco.fr>
2024-08-08 17:37:26 +0200

    feat: possibility to remove items from shopping list, ui: new logo

--- src/Controller/Utils/BooleanController.php
+++ src/Controller/Utils/BooleanController.php
@@ -7,11 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class BooleanController extends AbstractController
 {
 
     #[Route('/utils/boolean_toggle', name: 'utils_boolean_toggle', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function toggle(Request $request, EntityManagerInterface $em): JsonResponse
     {
         $data        = json_decode($request->getContent(), true);
@@ -22,6 +24,23 @@ class BooleanController extends AbstractController
         $setterName = "set" . ucfirst($payload['property']);
         $getterName = "get" . ucfirst($payload['property']);
         $entity->$setterName(!$entity->$getterName());
+        $em->flush();
+
+        return new JsonResponse();
+    }
+
+    #[Route('/utils/boolean_set', name: 'utils_boolean_set', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function set(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data        = json_decode($request->getContent(), true);
+        $raw_payload = $data['payload'];
+        $value       = boolval($data['value']);
+        $payload     = json_decode(base64_decode($raw_payload), true);
+
+        $entity     = $em->find($payload['class'], $payload['id']);
+        $setterName = "set" . ucfirst($payload['property']);
+        $entity->$setterName($value);
         $em->flush();
 
         return new JsonResponse();
--- src/Form/Config/AppearanceConfigType.php
+++ src/Form/Config/AppearanceConfigType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class AppearanceConfigType extends AbstractType
 {
